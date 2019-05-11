36b579f73f75b78bf447983bc29ca5e192edc596
KlaudiaMor <klaudia-moron@wp.pl>
2019-05-11 12:12:47 +0200

    Init repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostsComponent } from './posts/posts/posts.component';
 import { PostFormComponent } from './posts/post-form/post-form.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
 { path: '', component: PostsComponent },
